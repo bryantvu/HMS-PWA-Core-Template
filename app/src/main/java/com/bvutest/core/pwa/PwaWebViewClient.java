941e6e8ca36e52d0585ad2b033e477be9fdae101
Bryant Vu <bryantvu@gmail.com>
2021-01-28 17:16:04 -0800

    added URL whitelist filter

--- app/src/main/java/com/bvutest/core/pwa/PwaWebViewClient.java
+++ app/src/main/java/com/bvutest/core/pwa/PwaWebViewClient.java
@@ -1,13 +1,22 @@
 package com.bvutest.core.pwa;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.webkit.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 class PwaWebViewClient extends WebViewClient {
     private Pattern scope_pattern;
+    private ArrayList<Pattern> scopePatterns = new ArrayList<>();
+    private ArrayList<String> filters = new ArrayList<>(Arrays.asList("filterReplace"));
+//    private ArrayList<String> filters = new ArrayList<>(Arrays.asList(".ebay.com", ".ebayinc.com"));
+    private static final String TAG = "PwaWebViewClient";
 
     public PwaWebViewClient(String start_url, String scope) {
         try {
@@ -18,26 +27,75 @@ class PwaWebViewClient extends WebViewClient {
             }
 
             this.scope_pattern = this.regexFromPattern(scopeUrl.toString());
+            scopePatterns.add(this.regexFromPattern(scopeUrl.toString()));
+//            urlList.add(start_url);
         } catch (MalformedURLException e) {
             this.scope_pattern = null;
         }
+
+//        try{
+//            for(String item : urlList){
+//                URL baseUrl = new URL(item);
+//                URL scopeUrl = new URL(baseUrl, scope);
+//                scopeUrl = new URL(scopeUrl, "*");
+//                scopePatterns.add(this.regexFromPattern(scopeUrl.toString()));
+//            }
+//        }catch(Exception e){
+//            Log.d(TAG, "error adding URL to pattern array >> " + e);
+//        }
+
     }
 
     @SuppressWarnings("deprecation")
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
 //        if (this.scoped(url)) {
+//            Log.d(TAG, "shouldOverrideUrlLoading >> return false");
 //            return false;
 //        } else {
 //            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
 //            view.getContext().startActivity(i);
+//            Log.d(TAG, "shouldOverrideUrlLoading >> return true");
 //            return true;
 //        }
+        if (checkUrl(url)) {
+            Log.d(TAG, "shouldOverrideUrlLoading >> return false");
+            return false;
+        } else {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            view.getContext().startActivity(i);
+            Log.d(TAG, "shouldOverrideUrlLoading >> return true");
+            return true;
+        }
+//        return false;
+    }
+
+    private boolean checkUrl(String url){
+        for(String item : filters){
+            Log.d(TAG, "checkUrl >> " + url + " | " + item);
+            if(url.contains(item)){
+                Log.d(TAG, "checkUrl == match >> return true");
+                return true;
+            }
+        }
         return false;
     }
 
     private boolean scoped(String url) {
-        return this.scope_pattern == null || this.scope_pattern.matcher(url).matches();
+        if(this.scope_pattern == null ){
+            Log.d(TAG, "scoped == null >> return true");
+            return true;
+        }
+        for(Pattern item : scopePatterns){
+            Log.d(TAG, "scoped == matcher >> " + url + " | " + item.toString());
+            if(item.matcher(url).matches()){
+                Log.d(TAG, "scoped == match >> return true");
+                return true;
+            }
+        }
+        Log.d(TAG, "scoped != match >> return false");
+        return false;
+//        return this.scope_pattern.matcher(url).matches();
     }
 
     private Pattern regexFromPattern(String pattern) {
